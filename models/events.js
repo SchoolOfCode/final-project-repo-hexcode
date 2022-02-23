452c8b1144dca460b2e43c7c6b559f0e28861b6b
wisepanda <93228256+wisepanda@users.noreply.github.com>
2022-02-23 12:54:52 +0000

    joined events and appusers tables

--- models/events.js
+++ models/events.js
@@ -16,11 +16,34 @@ export async function getAllEvents() {
 
 export async function getEventById(eventId) {
     // VERSION 1: First, for basic test,  just do a simple select
-    const sqlString = `SELECT *
-        FROM event ev
-        WHERE ev.id = ${eventId};`; //TODO: refactor to use params and an array
+    // const sqlString = `SELECT *
+    //     FROM event ev
+    //     WHERE ev.id = ${eventId};`; //TODO: refactor to use params and an array
 
     // VERSION 2: TODO: then update to proper select (with join if one was needed) and aliases to match the variable names used in the front end
+
+    const sqlString = `SELECT
+            e.id as "id", 
+            e.event_title as "eventTitle",
+            e.event_description as "eventDescription",
+            e.event_location as "eventLocation",
+            e.event_date as "eventDate",
+            e.event_time as "eventTime",
+            e.event_requirements as "eventRequirements",
+            e.event_category as "eventCategory",
+            e.create_date_time as "eventCreateDateTime",
+            a.id as "organiserId",
+            a.app_user_first_name as "organiserFirstName",
+            a.app_user_last_name as "organiserLastName" ,
+            a.app_user_email as "organiserEmail" ,
+            a.app_user_profile_pic_link as "organiserProfilePicLink",
+            a.create_date_time as "organiserCreateDateTime"
+
+        FROM event e
+        INNER JOIN app_user a ON e.user_id = a.id
+        WHERE e.id = ${eventId};`;
+
+    
 
     console.log(`DEBUG: sqlString = ${sqlString}`);
 
