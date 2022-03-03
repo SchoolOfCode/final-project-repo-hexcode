93eb3689e3c7fdb36270dccab0c086312c9f0a17
nicfheorais <7238289+nicfheorais@users.noreply.github.com>
2022-03-03 11:38:52 +0000

    refactored getAllEventsForOneUser to get events that the user is invited to, not just ones they've organised

--- models/events.js
+++ models/events.js
@@ -45,32 +45,89 @@ export async function getAllEvents() {
 //       e.g.
 //       /appusers/:2/events/, where 2 is an app_user_id
 //********************************************************
-export async function getAllEventsForOneUser(appUserId) {
-    //TODO: this is wrong - this will only return events where the
-    // user is the organiser!!!
-    // NEED TO CREATE CONTACTS TABLE AND LINK OFF THAT.
-    const sqlString = `SELECT
-                            e.event_id as "eventId", 
-                            e.event_title as "eventTitle",
-                            e.event_description as "eventDescription",
-                            e.event_location as "eventLocation",
-                            to_char(e.event_date,'DD-MM-YYYY') as "eventDate",
-                            e.event_time as "eventTime",
-                            e.event_requirements as "eventRequirements",
-                            e.event_category as "eventCategory",
-                            e.event_create_date_time as "eventCreateDateTime",
-                            e.organiser_user_id as "organiserUserId",
-                            a.app_user_email as "organiserEmail",
-                            a.app_user_first_name as "organiserFirstName",
-                            a.app_user_last_name as "organiserLastName",
-                            a.app_user_profile_pic_link as "organiserProfilePicLink",
-                            a.app_user_create_date_time as "organiserCreateDateTime"
-                        FROM event e
-                        INNER JOIN app_user a ON e.organiser_user_id = a.app_user_id
-                        WHERE a.app_user_id = $1
-                        ORDER BY e.event_id DESC;`;
+export async function getAllEventsForOneUser(requestedUserId) {
+    // 02Mar2022 - REFACTORING to get all events where appUserId = organiser OR an invitee.
+    // can just select off invitee list BECAUSE we've edited the 'post event' to ALSO insert the logged in user into the invitee table.
+    // OLD join:
+    //     FROM event e
+    //     INNER JOIN app_user a ON e.organiser_user_id = a.app_user_id
+    //     WHERE a.app_user_id = $1
+    //     ORDER BY e.event_id DESC;`;
 
-    const sqlStringParams = [appUserId];
+    // TODO: this more complicated select, where i try and get the invitees' names
+    //        from the loggedin users's contact list, rather than the app users's table isn't working.
+    //       going to park it and go back to simpler select for now.
+
+    // const sqlString = `SELECT DISTINCT
+    //         e.event_id as "eventId",
+    //         e.event_title as "eventTitle",
+    //         e.event_description as "eventDescription",
+    //         e.event_location as "eventLocation",
+    //         to_char(e.event_date,'DD-MM-YYYY') as "eventDate",
+    //         e.event_time as "eventTime",
+    //         e.event_requirements as "eventRequirements",
+    //         e.event_category as "eventCategory",
+    //         e.event_create_date_time as "eventCreateDateTime",
+
+    //         e.organiser_user_id as "organiserUserId",
+    //         a.app_user_first_name as "organiserFirstName",
+    //         a.app_user_last_name as "organiserLastName",
+    //         a.app_user_email as "organiserEmail",
+    //         a.app_user_profile_pic_link as "organiserProfilePicLink",
+    //         a.app_user_create_date_time as "organiserCreateDateTime",
+
+    //         i.invitee_user_id as "inviteeUserId",
+    //         c.contact_name as "inviteeContactName",
+    //         b.app_user_first_name as "inviteeAppUserFirstName",
+    //         b.app_user_last_name as "inviteeAppUserLastName",
+    //         b.app_user_email as "inviteeEmail",
+    //         i.event_invitee_rsvp_status as "inviteeRsvpStatus",
+    //         i.invite_issuer_user_id as "inviteIssuerUserId",
+    //         to_char(i.event_invitee_rsvp_logged_date_time,'DD-MM-YYYY HH:MM:SS') as "eventInviteeRsvpLoggedDateTime",
+    //         i.event_invitee_create_date_time as "eventInviteeCreateDateTime",
+    //         i.event_invitee_id as "eventInviteeId"
+
+    //     FROM event e
+    //     INNER JOIN app_user a ON e.organiser_user_id = a.app_user_id
+    //     INNER JOIN event_invitee i ON i.event_id = e.event_id
+    //     INNER JOIN app_user b ON i.invitee_user_id = b.app_user_id
+    //     INNER JOIN contact c ON  i.invitee_user_id = c.contact_user_id
+    //                          AND i.invite_issuer_user_id = c.contact_owner_user_id
+    //     WHERE i.invitee_user_id = $1
+    //     ORDER BY e.event_id DESC;`;
+
+    // TEMP: simpler select to get all events for the loggedin user where they are the organiser or invitee.
+    const sqlString = `SELECT DISTINCT
+            e.event_id as "eventId", 
+            e.event_title as "eventTitle",
+            e.event_description as "eventDescription",
+            e.event_location as "eventLocation",
+            to_char(e.event_date,'DD-MM-YYYY') as "eventDate",
+            e.event_time as "eventTime",
+            e.event_requirements as "eventRequirements",
+            e.event_category as "eventCategory",
+            e.event_create_date_time as "eventCreateDateTime",
+            e.organiser_user_id as "organiserUserId",
+
+            a.app_user_first_name as "organiserFirstName",
+            a.app_user_last_name as "organiserLastName",
+            a.app_user_email as "organiserEmail",
+            a.app_user_profile_pic_link as "organiserProfilePicLink",
+            a.app_user_create_date_time as "organiserCreateDateTime",
+
+            i.invitee_user_id as "inviteeUserId",
+            b.app_user_email as "inviteeEmail",
+            i.event_invitee_rsvp_status as "inviteeRsvpStatus"
+
+        FROM event e
+        INNER JOIN app_user a ON e.organiser_user_id = a.app_user_id
+        INNER JOIN event_invitee i ON i.event_id = e.event_id
+        INNER JOIN app_user b ON i.invitee_user_id = b.app_user_id
+        
+        WHERE i.invitee_user_id = $1
+        ORDER BY e.event_id DESC;`;
+
+    const sqlStringParams = [requestedUserId];
 
     debugOut(
         `/models/events.js - getAllEventsForOneUser`,
